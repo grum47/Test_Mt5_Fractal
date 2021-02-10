3261d49389540071e9287b7f0de877dcea3268f1
grum47 <2if-kPL-yET-mCU>
2021-02-10 20:27:05 +0300

    Добился последовательного перебора тиков после того, как "выставился" фрактал. Перебор осущесвляется очень медленно. Есть идея перебирать бары М1, до тех пор пока high не станет <= 10пунктов от точки фрактала, с этого момента начать перебирать тики.
    ЛИБО:
    В случае, если high минутного бара пересечет точку фрактала - можно начать перебор тиков со времени начала образования предыдущей минутной свечи.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 from datetime import datetime
 import pytz
 
-
 mt5.initialize()
 if not mt5.initialize():
     print("Инициализация не прошла. Ошибка - ", mt5.last_error())
@@ -15,6 +14,7 @@ pd.set_option('display.max_columns', 500)  # Количество столбцо
 pd.set_option('display.max_rows', 1000)  # Количество строк
 pd.set_option('display.width', 1500)  # Макс. ширина таблицы для показа
 timezone = pytz.timezone("Etc/UTC")
+
 
 def get_rates():
     """
@@ -30,6 +30,7 @@ def get_rates():
 
 # Выводим на печать массив значений баров в виде таблицы
 rates_frame = pd.DataFrame(get_rates())
+rates_frame['time'] = pd.to_datetime(rates_frame['time'], unit='s')
 print(rates_frame)
 
 
@@ -49,56 +50,86 @@ print("high = ", high)
 
 
 def fractal_up(high):
-    for _ in rates:
+    for candle in rates:
         if rates[high + 2][2] > rates[high + 1][2] and \
                 rates[high + 2][2] > rates[high][2] and \
                 rates[high + 2][2] >= rates[high + 3][2] and \
                 rates[high + 2][2] >= rates[high + 4][2]:
             print("Есть фрактал вверх. Точка фрактала = ", rates[high + 2][2])
+            print(candle)
             flag_by_fractal_up = True
             print("Flag Fractal BUY = ", flag_by_fractal_up)
             return rates[high + 2][2], flag_by_fractal_up, rates[high + 5][0], \
                    rates[last_index() + len(get_rates()) - 1][0]
         else:
             print("Нет фрактала")
+            # print(candle)
             flag_by_fractal_up = False
             print("Flag Fractal BUY = ", flag_by_fractal_up)
             # print("Нет фрактала", rates[n + 2][2])
             high += 1
 
 
-buy_price = fractal_up(high)
+buy_price = fractal_up(high)[0]
+flag_by_fractal_up = fractal_up(high)[1]
+date_from_for_ticks = int(fractal_up(high)[2])  # datetime.strptime(str(buy_price[2]), "%Y.%m.%d")
+date_to_for_ticks = int(fractal_up(high)[3])  # datetime.strptime(str(buy_price[3]), "%Y.%m.%d")
 print("Проверка возврата значений функции fractal_up()")
-print("BUY PRICE = ", buy_price[0])
-print("Flag BUY - ", buy_price[1])
-print("Date from ticks, sec = ", buy_price[2])
-print("Date to ticks, sec = ", buy_price[3])
+print("BUY PRICE = ", buy_price)
+print("Flag BUY - ", flag_by_fractal_up)
+print("Date from ticks, sec = ", date_from_for_ticks)
+print("Date to ticks, sec = ", date_to_for_ticks)
 
-
-date_from_for_ticks = int(buy_price[2])             #datetime.strptime(str(buy_price[2]), "%Y.%m.%d")
-date_to_for_ticks = int(buy_price[3])               #datetime.strptime(str(buy_price[3]), "%Y.%m.%d")
 print("Проверка присваивания дат переменным")
-print("Дата от - ", date_from_for_ticks)
-print("Дата до - ", date_to_for_ticks)
+print("Дата от - ", date_from_for_ticks, datetime.fromtimestamp(date_from_for_ticks-60*60*3))
+print("Дата до - ", date_to_for_ticks, datetime.fromtimestamp(date_to_for_ticks-60*60*3))
 
 
 def get_price():
-    ticks_last = mt5.copy_ticks_from(symbol, date_from_for_ticks, date_to_for_ticks, mt5.COPY_TICKS_INFO)
+    ticks_last = mt5.copy_ticks_range(symbol, date_from_for_ticks, date_to_for_ticks, mt5.COPY_TICKS_INFO)
     """
     if ticks_last == None:
         print(mt5.last_error())"""
-    print("Получено тиков: ", len(ticks_last))
+    # for ticks in ticks_last:
+    # print("Tick Last Price = ", ticks[3])
     return ticks_last
 
 
-get_price()
+print(len(get_price()))
+index_raws = 0
+index_columns = 3
+last_tick = get_price()[index_raws][index_columns]
+print(last_tick)
+print("ТИП LAST_TICK", last_tick, type(last_tick))
+print("ТИП BUY_PRICE", buy_price, type(buy_price))
+# print(get_price())
+# get_price()
 # Выводим на печать массив значений тиков в виде таблицы
 ticks_frame = pd.DataFrame(get_price())
 ticks_frame['time'] = pd.to_datetime(ticks_frame['time'], unit='s')
 print(ticks_frame)
 
-"""if buy_price[1] == True:
-    print("True")
-else:
-    print("False")
-"""
+
+# print(get_price()[3])
+
+
+def ticking_over():
+    index_raws = 0
+    index_columns = 3
+    last_tick = get_price()[index_raws][index_columns]
+    for ticks in get_price():
+        if flag_by_fractal_up:
+            print("Начинаем перебор тиков")
+            if last_tick >= buy_price:
+                print("НАКОНЕЦ-ТО БЛЯТЬ", last_tick, buy_price, ticks)
+                break
+            else:
+                print("нет активации", last_tick, buy_price)
+                index_raws += 1
+                last_tick = get_price()[index_raws][index_columns]
+                print(index_raws, index_columns)
+        else:
+            print("False")
+
+
+ticking_over()
